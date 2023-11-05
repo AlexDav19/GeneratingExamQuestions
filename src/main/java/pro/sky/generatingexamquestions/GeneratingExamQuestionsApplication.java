9f1125cd4edd366af7b3f51c61c8efcbf3d17330
aleks <aleksei-davletov@mail.ru>
2023-11-05 22:19:27 +0300

    Initial commit

---
+++ src/main/java/pro/sky/generatingexamquestions/GeneratingExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.generatingexamquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratingExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratingExamQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/generatingexamquestions/GeneratingExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.generatingexamquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratingExamQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
